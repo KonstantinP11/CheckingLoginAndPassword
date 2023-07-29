1f9d3dcf86a524bab30b6d499a5a85efe4a37d58
conditional <105pkn@mail.ru>
2023-07-29 21:16:25 +0700

    init commit

---
+++ src/test/java/com/example/checkingloginandpassword/CheckingLoginAndPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkingloginandpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingLoginAndPasswordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
